32b23e73835321fb21d43899c6c5fd7c07b435cb
danghung <hungdang02042003@gmail.com>
2023-07-02 06:33:19 +0700

    [feat/be]: authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import categoryRoutes from './category.routes.js';
 import express from 'express';
+import userRoutes from './auth.routes.js';
 
 const router = express.Router();
 
-const rootRoutes = [categoryRoutes];
+const rootRoutes = [categoryRoutes, userRoutes];
 
 rootRoutes.map((route) => {
   router.use(route);
