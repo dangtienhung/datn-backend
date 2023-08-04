8eecb609daae3d4044ddebcecd26a0173fcd05e0
long1703@outlook.com <long1703@outlook.com>
2023-08-04 22:58:31 +0700

    update

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get('/categories', categoryController.getAll);
 router.get('/category/:id', categoryController.getOne);
 router.post('/category', categoryController.create);
+router.delete('/category/:id', categoryController.fakeDelete);
+router.put('/category/:id', categoryController.update);
 
 export default router;
