fc8c6889d8b2ddeb2019324e3b67a7eaa601983f
thanhdo21d <thanhdo9xi@gmail.com>
2023-08-28 22:36:57 +0700

    done bot

--- src/chat.js
+++
@@ -1,7 +0,0 @@
-
-export const isAdmin = async (req, res, next) => {
-  const { email } = req.user;
-  // const adminUser = await User.findOne({ email });
-  res.send(email)
-  console.log(req.body.email)
-}
