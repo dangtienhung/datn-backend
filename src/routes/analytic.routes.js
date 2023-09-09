e48641726dfeadd28951b5ababddae15eed133f0
cô giáo hưng <hungdang02042003@gmail.com>
2023-09-09 23:30:50 +0700

    feat: analytic order/user/voucher

--- src/routes/analytic.routes.js
+++ src/routes/analytic.routes.js
@@ -3,7 +3,13 @@ import express from 'express';
 
 const router = express.Router();
 
+/* order */
 router.get('/countVoucher', analyticController.countVoucher);
-router.get('/countVoucherExpiration', analyticController.countVoucherExpiration);
+router.get('/countOrder', analyticController.countOrder);
+router.get('/countOrderWeek', analyticController.countOrderWeek);
+/* đếm số order theo sản phẩm */
+router.get('/countOrderDayByProduct', analyticController.countOrderDayByProduct);
+/* đếm số lượng người dùng */
+router.get('/countUser', analyticController.countUser);
 
 export default router;
