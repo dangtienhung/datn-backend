2e37332205234f1547c3f6639a6a160636b232f3
thanhdo21d <thanhdo9xi@gmail.com>
2023-10-11 19:46:33 +0700

    connect server

--- bot/Chatbot/index.js
+++ bot/Chatbot/index.js
@@ -17,11 +17,14 @@ manager.train().then(async () => {
     res.json(response);
 
     //success
+
     // res.send(response.answer || 'Xin lỗi , thông tin không có sẵn , vui lòng chuyển sang câu hỏi khác');
   });
   // console.log( await manager.process('vi',"hello"));
   // app.listen(3000);//
 });
+
+//connect serrver
 mongoose.connect(
   'mongodb+srv://hungdang02042003:jVp9aHU2eqE747nE@du-an-framework2-milk-t.ntg5d7s.mongodb.net/?retryWrites=true&w=majority',
   {
