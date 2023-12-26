17f124d9871e31a590689d0ace846a8aa785cef3
dev.fintwin <hoanglonggg356@gmail.com>
2023-12-26 10:37:50 +0700

    update

--- src/app.js
+++ src/app.js
@@ -101,6 +101,7 @@ passport.use(passportMiddleware.GoogleAuth);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://milk-tea-connect.click");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
